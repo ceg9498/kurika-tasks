b08cd206dbe1f9d804468345fa200a84b432c578
C.E. Gagne <ceg9498@gmail.com>
2019-11-24 02:38:07 -0500

    Timers display
    Mostly done. Restructured the timers service data storage to make them easy to display.

--- src/app/timers.service.ts
+++ src/app/timers.service.ts
@@ -7,40 +7,58 @@ import { timer, Timer } from '../types/timer';
   providedIn: 'root'
 })
 export class TimersService {
-  timerList:timer[];
+  categories:[{category:string, timers:timer[]}];
 
   constructor(
     private _idb:IndexedDbService,
   ) { }
 
   init(){
-    this.timerList = [];
+    this.categories = [{
+      category: "No Category",
+      timers: []
+    }];
     this._idb.getData("kurika","timers").subscribe(
       (res) => {
-        this.timerList.push(
-          new Timer(
-            res.title, 
-            res.required, 
-            res.isCompleted, 
-            res.period, 
-            res.description, 
-            res.category
-          )
-        );
+        this.setTimer(res);
       },
       (err) => {
         console.error("Error retrieving timer data!",err);
       },
       () => {
-        console.log("Timers retrieval complete.")
+        console.log("Timers retrieval complete.");
+        this.cleanCategories();
       }
     )
+  }
+
+  setTimer(timer:timer){
+    if(timer.category === undefined || timer.category === ''){
+      timer.category = "No Category";
+    }
+    let i = this.categories.findIndex((category)=> category.category === timer.category);
+    if(i === -1){
+      this.categories.push({
+        category: timer.category,
+        timers: [timer]
+      });
+    } else {
+      this.categories[i].timers.push(timer);
+    }
+  }
+
+  cleanCategories(){
+    this.categories.forEach((category, i)=>{
+      if(category.timers.length === 0){
+        this.categories.splice(i, 1);
+      }
+    })
   }
 
   addTimer(title:string, required:boolean, isCompleted:boolean, period:string, description?:string, category?:string) {
     let nTimer = new Timer(title, required, isCompleted, period, description, category);
     console.log(nTimer);
-    this.timerList.push(nTimer);
+    this.setTimer(nTimer);
     this._idb.addOrUpdateOne("kurika", "timers", nTimer).subscribe(
       (res) => {
         console.log("Saved timer result:",res);
@@ -55,6 +73,14 @@ export class TimersService {
   }
 
   deleteTimer(id:string){
+    this.categories.forEach((item, i)=>{
+      item.timers.forEach((timer, j)=>{
+        if(timer.id === id){
+          this.categories[i].timers.splice(j,1);
+          this.cleanCategories();
+        }
+      });
+    });
     this._idb.deleteOne("kurika", "timers", id).subscribe(
       (res) => {
         console.log("Delete timer result:",res);
@@ -66,5 +92,35 @@ export class TimersService {
         console.log("Delete timer completed.");
       }
     );
+  }
+
+  getTimers(){
+    return this.categories;
+  }
+
+  toggleCompleted(id:string){
+    this.categories.forEach((item, i)=>{
+      item.timers.forEach((timer, j)=>{
+        if(timer.id === id){
+          this.categories[i].timers[j].isCompleted = !this.categories[i].timers[j].isCompleted;
+          if(this.categories[i].timers[j].isCompleted){
+            this.categories[i].timers[j].completed.push(new Date());
+          } else {
+            this.categories[i].timers[j].completed.shift();
+          }
+          this._idb.addOrUpdateOne("kurika", "timers", this.categories[i].timers[j]).subscribe(
+            (res)=>{
+              console.log("Update result:",res);
+            },
+            (err)=>{
+              console.log("Update error:",err);
+            },
+            () => {
+              console.log("Update complete");
+            }
+          )
+        }
+      });
+    });
   }
 }
--- src/app/timers/timers.component.ts
+++ src/app/timers/timers.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TimersService } from '../timers.service';
+import { timer, Timer } from '../../types/timer';
+
 @Component({
   selector: 'app-timers',
   templateUrl: './timers.component.html',
   styleUrls: ['./timers.component.scss']
 })
 export class TimersComponent implements OnInit {
+  timers:[{category:string, timers:timer[]}]
 
-  constructor() { }
+  constructor(
+    private _timers:TimersService,
+  ) { }
 
   ngOnInit() {
+    this.getTimers();
   }
 
+  getTimers(){
+    this.timers = this._timers.getTimers();
+  }
+
+  deleteTimer(id:string){
+    this._timers.deleteTimer(id);
+  }
+
+  toggleCompleted(id:string){
+    this._timers.toggleCompleted(id);
+  }
 }
