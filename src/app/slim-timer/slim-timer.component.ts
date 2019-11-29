bade4844b42300f4edf20a92a970f5421ba0f169
C.E. Gagne <ceg9498@gmail.com>
2019-11-29 01:04:55 -0500

    Fix bugs with slim timers component

--- src/app/slim-timer/slim-timer.component.ts
+++ src/app/slim-timer/slim-timer.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
+import { TimersService } from '../timers.service';
+import { Timer } from '../../types/timer';
+
 @Component({
   selector: 'app-slim-timer',
   templateUrl: './slim-timer.component.html',
   styleUrls: ['./slim-timer.component.scss']
 })
 export class SlimTimerComponent implements OnInit {
-  @Input() timers;
+  @Input() timers:Timer[];
   @Input() hideCompleted:boolean;
 
-  constructor() { }
+  constructor(
+    private _timers:TimersService,
+  ) { }
 
   ngOnInit() {
   }
 
+  showInfo(id:string|number) {
+    console.log("Show info for timer:",id)
+  }
+
+  toggleCompleted(id:string){
+    this._timers.toggleCompleted(id);
+  }
 }
